88841121dfe204a92ceb77cd4e4f8089da3bce6a
nisha <nisha@LAPTOP-DQRODS74>
2024-01-08 16:05:06 +0530

    test commit

--- src/test/java/TestCasses/AddTocart2Test.java
+++ src/test/java/TestCasses/AddTocart2Test.java
@@ -9,7 +9,7 @@ public class AddTocart2Test extends BaseClass{
 	@Test
 	public void Tc_001() {
 		System.out.println("first test case");
-		
+		/*test comment*/
 		String Name = System.getProperty("name");
 		System.out.println(Name);
 		System.out.println(Name);
